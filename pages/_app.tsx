d56c2f42687713fd2e9da45ac13c90f5b01ee9b1
Pradeep Kaswan <76137397+kaswanp1406@users.noreply.github.com>
2022-11-06 01:35:12 +0530

    Format code style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,15 +5,15 @@ import "reset-css";
 const theme = extendTheme({
   colors: {
     gray: {
-      100: '#F5f5f5',
-      200: '#EEEEEE',
-      300: '#E0E0E0',
-      400: '#BDBDBD',
-      500: '#9E9E9E',
-      600: '#757575',
-      700: '#616161',
-      800: '#424242',
-      900: '#212121',
+      100: "#F5f5f5",
+      200: "#EEEEEE",
+      300: "#E0E0E0",
+      400: "#BDBDBD",
+      500: "#9E9E9E",
+      600: "#757575",
+      700: "#616161",
+      800: "#424242",
+      900: "#212121",
     },
   },
   components: {
@@ -37,7 +37,7 @@ function MyApp({ Component, pageProps }) {
         <Component {...pageProps} />
       </PlayerLayout>
     </ChakraProvider>
-  )
-};
+  );
+}
 
 export default MyApp;
