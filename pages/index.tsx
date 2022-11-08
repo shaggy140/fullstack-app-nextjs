7545f2226c86059a10a3f15b288ba23baa75c76e
Pradeep Kaswan <76137397+kaswanp1406@users.noreply.github.com>
2022-11-08 17:38:12 +0530

    added playlist pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,55 @@
+import { Box, Text, Flex } from "@chakra-ui/layout";
+import { Image } from "@chakra-ui/react";
+import prisma from "../lib/prisma";
 import GradientLayout from "../components/gradientLayout";
+import { useMe } from "../lib/hooks";
 
-const Home = () => {
+const Home = ({ artists }) => {
+  const { user } = useMe();
+
   return (
     <GradientLayout
       roundImage
-      color="blue"
+      color="gray"
       subtitle="profile"
-      title="Pradeep Kaswan"
-      description="15 public playlist"
+      title={`${user?.firstName} ${user?.lastName}`}
+      description={`${user?.playlistsCount} public playlists`}
       image="https://imgix.ranker.com/user_node_img/50087/1001738942/original/human-relationships-photo-u1?auto=format&q=60&fit=crop&fm=pjpg&dpr=2&w=375"
     >
-      <div>home page</div>
+      <Box color="white" paddingX="40px">
+        <Box marginBottom="40px">
+          <Text fontSize="2xl" fontWeight="bold">
+            Top artist this month
+          </Text>
+          <Text fontSize="md">only visible to you</Text>
+        </Box>
+        <Flex>
+          {artists.map((artist) => (
+            <Box paddingX="10px" width="20%">
+              <Box bg="gray.900" borderRadius="4px" padding="15px" width="100%">
+                <Image
+                  src="https://placekitten.com/g/300/300"
+                  borderRadius="100%"
+                />
+                <Box marginTop="20px">
+                  <Text fontSize="large">{artist.name}</Text>
+                  <Text fontSize="x-small">Artist</Text>
+                </Box>
+              </Box>
+            </Box>
+          ))}
+        </Flex>
+      </Box>
     </GradientLayout>
   );
+};
+
+export const getServerSideProps = async () => {
+  const artists = await prisma.artist.findMany({});
+
+  return {
+    props: { artists },
+  };
 };
 
 export default Home;
